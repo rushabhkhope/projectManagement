00a88f68a8a0cdb2fe0f76c92610326bb919f725
rawasaditya <rawasaditya@gmail.com>
2023-10-02 19:29:49 +0530

    added swagger to present apis

--- backend/server.js
+++ backend/server.js
@@ -3,21 +3,22 @@ const app = express();
 const mongoose = require("mongoose");
 const authRoutes = require("./routes/authRoutes");
 const PORT = process.env.PORT || 5000;
+const swagger = require("./swagger");
 app.use(express.json());
 app.listen(PORT, () => {
-  // console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
   mongoose
     .connect(
       "mongodb://0.0.0.0:27017/pms?readPreference=primary&directConnection=true&ssl=false"
     )
     .then((data) => {
-      // console.log("mongodb connected");
+      console.log("mongodb connected");
     })
     .catch((err) => {
-      // console.log(err);
+      console.log(err);
     });
 });
 
 app.use("/api/auth", authRoutes);
-
+swagger(app);
 module.exports = app;
---
+++ backend/swagger.js
@@ -0,0 +1,26 @@
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
+const options = {
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Your API Documentation",
+      version: "1.0.0",
+      description: "API documentation for your application",
+    },
+    servers: [
+      {
+        url: "http://localhost:5000",
+        description: "Development server",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+
+const specs = swaggerJsdoc(options);
+
+module.exports = (app) => {
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
+};
